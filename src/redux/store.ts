7b76d361578cb9dce100b4c13c9bae37fb152a5d
Olexiy Bashmakov <olexiybashmakov@gmail.com>
2021-07-01 12:17:35 +0300

    first commit

---
+++ src/redux/store.ts
@@ -0,0 +1,17 @@
+import { applyMiddleware, createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './rootReducer';
+
+export default function configureStore() {
+  const middlewares = [thunkMiddleware];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+
+  const enhancers = [middlewareEnhancer];
+  const composedEnhancers = composeWithDevTools(...enhancers);
+
+  const store = createStore(rootReducer, composedEnhancers);
+
+  return store;
+}
